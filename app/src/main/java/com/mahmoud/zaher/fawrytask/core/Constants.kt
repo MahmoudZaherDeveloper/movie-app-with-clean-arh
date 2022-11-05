9cda8f7fbc4777c0cfa8a9adcc70e058c4d38090
MahmoudZaher <mahmoudzaher.developer@gmail.com>
2022-11-05 15:00:16 +0200

    add local module DI

--- app/src/main/java/com/mahmoud/zaher/fawrytask/core/Constants.kt
+++ app/src/main/java/com/mahmoud/zaher/fawrytask/core/Constants.kt
@@ -1,3 +1,4 @@
 package com.mahmoud.zaher.fawrytask.core
 
 const val BASE_URL = "https://api.themoviedb.org/"
+const val DB_NAME = "movie-db"
---
+++ app/src/main/java/com/mahmoud/zaher/fawrytask/data/sources/di/LocalModule.kt
@@ -0,0 +1,30 @@
+package com.mahmoud.zaher.fawrytask.data.sources.di
+
+import android.content.Context
+import androidx.room.Room
+import com.mahmoud.zaher.fawrytask.core.DB_NAME
+import com.mahmoud.zaher.fawrytask.data.sources.local.MovieDao
+import com.mahmoud.zaher.fawrytask.data.sources.local.MovieDatabase
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@HiltAndroidApp
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+
+    @Provides
+    @Singleton
+    fun provideDao(movieDB: MovieDatabase): MovieDao {
+        return movieDB.movieDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieDB(@ApplicationContext appContext: Context): MovieDatabase {
+        return Room.databaseBuilder(appContext, MovieDatabase::class.java, DB_NAME).build()
+    }
+}
