b3f75adfed34434c155e2024fe748b7651fff4b3
MahmoudZaher <mahmoudzaher.developer@gmail.com>
2022-11-05 15:42:09 +0200

    finish domain layer

---
+++ app/src/main/java/com/mahmoud/zaher/fawrytask/domain/interactors/MovieInteractor.kt
@@ -0,0 +1,11 @@
+package com.mahmoud.zaher.fawrytask.domain.interactors
+
+import com.mahmoud.zaher.fawrytask.domain.model.Movie
+import com.mahmoud.zaher.fawrytask.domain.repository.MovieRepository
+import javax.inject.Inject
+
+class MovieInteractor @Inject constructor(val movieRepository: MovieRepository) {
+    suspend fun getMovieList(): List<Movie> {
+        return movieRepository.getMovieList()
+    }
+}
