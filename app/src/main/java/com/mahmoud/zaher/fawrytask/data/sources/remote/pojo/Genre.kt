c15a7ea6f50716b47ab7ecddacc238ec01691b83
MahmoudZaher <mahmoudzaher.developer@gmail.com>
2022-11-05 14:38:55 +0200

    add pojo classes

---
+++ app/src/main/java/com/mahmoud/zaher/fawrytask/data/sources/remote/pojo/Genre.kt
@@ -0,0 +1,9 @@
+package com.mahmoud.zaher.fawrytask.data.sources.remote.pojo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/mahmoud/zaher/fawrytask/data/sources/remote/pojo/SpokenLanguage.kt
@@ -0,0 +1,12 @@
+package com.mahmoud.zaher.fawrytask.data.sources.remote.pojo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SpokenLanguage(
+    @SerializedName("english_name")
+    val englishName: String,
+    @SerializedName("iso_639_1")
+    val iso6391: String,
+    val name: String
+)
